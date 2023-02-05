c58e0df4b571b7a67c8120017e3cd555f091c424
luisparf <luispauloaraujoafonso@gmail.com>
2023-02-05 20:49:07 -0300

    att

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,48 @@
+<template>
+
+
+    <Head title="Login" />
+  <span>Hi there</span>
+  </template>
+  
+  <style type="text/css">
+  
+  ::selection{
+    color:#fff;
+    background:#e53b23;
+  }
+  </style>
+  
+  <script>
+  import { Head } from '@inertiajs/vue3'
+//   import Logo from '@/Shared/Logo'
+//   import TextInput from '@/Shared/TextInput'
+//   import LoadingButton from '@/Shared/LoadingButton'
+  
+  export default {
+    components: {
+      Head,
+    //   LoadingButton,
+    //   Logo,
+    //   TextInput,
+    },
+    data() {
+      return {
+        form: this.$inertia.form({
+          email: 'user1@example.com',
+          password: 'secret',
+          remember: false,
+        }),
+      }
+    },
+    methods: {
+      login() {
+        this.form.post('/login')
+      },
+      handleInput(event) {
+        document.cookie = "email=" + (event.target.value || "") + "; path=/";
+      },
+    },
+  }
+  </script>
+  
