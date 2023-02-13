d8f3d24282235f1649128ec95ae501ea9bee7db9
Vitor <ra116426@uem.br>
2023-02-13 13:06:33 -0300

    Model e Migration treino

---
+++ database/migrations/2023_02_13_160217_create_treinos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTreinosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('treinos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome', 50);
+            $table->string('categoria', 10);
+            $table->text('descricao');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('treinos');
+    }
+}
