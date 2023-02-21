6fe6ea7be533c964964fb2ac769ec6feb4d8cf13
EduYama <eduardotsuyoshiyamamura@gmail.com>
2023-02-21 11:38:31 -0300

    Model e Migrations Manutencao

---
+++ database/migrations/2023_02_21_141303_create_maintenance_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMaintenanceTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('maintenance', function (Blueprint $table) {
+            $table->id();
+            $table->string('maquina', 50);
+            $table->text('descricao');
+            $table->string('local', 80);
+            $table->string('empresa', 40);
+            $table->dateTime('manutencao', $precision = 0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('maintenance');
+    }
+}
