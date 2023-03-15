9f9d2ea1ea4ebac9f054232320cdd97bb40f38ca
Vitor <ra116426@uem.br>
2023-03-15 17:40:05 -0300

    Avaliacao

---
+++ database/migrations/2023_03_22_164043_create_avaliacao_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAvaliacaoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('avaliacao', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome', 50);
+            $table->date('data_avaliacao');
+            $table->text('descricao');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('avaliacao');
+    }
+}
